22fa2c1e1011ba8725b3796628fed19a53cfaa1d
AnanthanK <kanagapa@mcmaster.ca>
2017-12-06 11:56:35 -0500

    added ActiveTransactions activity and navigation from main to activetransaction activities

--- app/src/main/java/com/example/cocoabutter/split/MainActivity.java
+++ app/src/main/java/com/example/cocoabutter/split/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, ActiveTransactions.class);
+        startActivity(intent);
     }
 
     @Override
